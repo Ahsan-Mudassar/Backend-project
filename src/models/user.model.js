557251cabd47cf057e9da767dc379f7693341b25
Ahsan-Mudassar <ahsanmudassar441@gmail.com>
2026-07-28 01:41:00 +0500

    add await in fnc

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,11 +22,10 @@ const userSchema = new Schema({
         type: String,
         required: [true, "fullName is required"],
         lowercase: true,
-        unique: true,
         trim: true,
         index: true
     },
-    avater: {
+    avatar: {
         type: String,
         required: true
     },
@@ -55,7 +54,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next();
 })
 
