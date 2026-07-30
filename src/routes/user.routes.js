9a2bb044f9b7e1aead51dc66b50f0afd32ea4a81
Ahsan-Mudassar <ahsanmudassar441@gmail.com>
2026-07-30 16:57:16 +0500

    remove secrets from user.router.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,10 +7,3 @@ router.route("/register").post(registerUser)
 
 export default router
 
-
-
-
-
-
-// # MONGODB_URI=mongodb://youtubetest:Ahsan123@ac-wjyi2vz-shard-00-00.suv0cgc.mongodb.net:27017,ac-wjyi2vz-shard-00-01.suv0cgc.mongodb.net:27017,ac-wjyi2vz-shard-00-02.suv0cgc.mongodb.net:27017/?replicaSet=atlas-t4ppjz-shard-0&ssl=true&authSource=admin
-// # MONGODB_URI=mongodb+srv://youtubetest:Ahsan123@cluster0.suv0cgc.mongodb.net
